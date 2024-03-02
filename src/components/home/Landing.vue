3616a2d904c25d4c6b05517d7bb7b0f55b56e960
Jwu488 <joshuawu158@gmail.com>
2024-03-02 14:47:57 -0600

    janwdjdnkjdaw

--- src/components/home/Landing.vue
+++ src/components/home/Landing.vue
@@ -3,5 +3,10 @@
 </script>
 
 <template>
-    <img src=""></img>
+    <img class="z-0 absolute h-fit" src="../../assets/LandingPage.png" alt="image of AME23"></img>
+
+    <div class="relative z-10 pt-8 ps-8">
+        <p1 class="text-white font-semibold text-3xl">Texas A&M Formula SAE EV 2024</p1>
+        <button class="bg-color"></button>
+    </div>
 </template>
