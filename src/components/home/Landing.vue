eca51fa8931b1e3972950e24d7be283fcf1e4e8a
Jwu488 <joshuawu158@gmail.com>
2024-03-02 15:38:43 -0600

    nlnldwalnjdwa

--- src/components/home/Landing.vue
+++ src/components/home/Landing.vue
@@ -12,19 +12,22 @@
         </button>
     </div> -->
 
-    <div class="bg-[url('@/assets/LandingPage.png')] bg-contain w-screen h-screen flex mx-auto">
-        <div class="pt-12 pl-12 flex-col flex">
+    <div class="bg-[url('@/assets/LandingPage50.png')] bg-contain w-screen h-screen flex mx-auto bg-no-repeat bg-opacity-50">
+        <div class="pt-96 pl-12 flex-col flex">
             <p1 class="text-white font-semibold text-4xl pb-2">
                 Texas A&M Formula SAE EV 2024
             </p1>
+            <p1 class="pb-3 text-gray-200">
+                "Design for testing time and quality."
+            </p1>
             <div class="flex-row flex">
                 <div class="pr-4">
-                    <button class="bg-white rounded-lg h-fit py-3 w-fit px-8 text-l font-medium flex">
-                        Join Our Mailing List
+                    <button class="bg-white rounded-2xl h-fit py-3 w-fit px-8 text-l font-medium flex">
+                        Sponsor or Donate
                     </button>
                 </div>
-                <button class="outline rounded-lg h-fit py-3 w-fit px-8 text-l font-medium flex">
-                    Sponsor or Donate
+                <button class="outline rounded-2xl h-fit py-3 w-fit px-8 text-l font-medium flex color-white text-white">
+                    Join Our Mailing List
                 </button>
             </div>
         </div>
