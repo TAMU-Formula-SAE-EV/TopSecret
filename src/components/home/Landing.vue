36c45aba808fd671e1b7837abf60b4869af845ac
Jwu488 <joshuawu158@gmail.com>
2024-03-02 15:22:46 -0600

    nkankjwdnja

--- src/components/home/Landing.vue
+++ src/components/home/Landing.vue
@@ -3,10 +3,33 @@
 </script>
 
 <template>
-    <img class="z-0 absolute h-fit" src="../../assets/LandingPage.png" alt="image of AME23"></img>
+    <!-- <img class="z-0 relative h-fit" src="../../assets/LandingPage.png" alt="image of AME23"></img>
 
-    <div class="relative z-10 pt-8 ps-8">
+    <div class="relative z-10 pt-8 ps-8 ">
         <p1 class="text-white font-semibold text-3xl">Texas A&M Formula SAE EV 2024</p1>
-        <button class="bg-color"></button>
+        <button class="bg-aggie-maroon">
+            hi
+        </button>
+    </div> -->
+
+    <div class="bg-[url('@/assets/LandingPage.png')] bg-contain w-screen h-screen flex mx-auto">
+        <div class="pt-12 pl-12 flex-col flex">
+            <p1 class="text-white font-semibold text-4xl pb-2">
+                Texas A&M Formula SAE EV 2024
+            </p1>
+            <div class="flex-row flex">
+                <div class="pr-4">
+                    <button class="bg-white rounded-lg h-fit py-3 w-fit px-8 text-l font-medium flex">
+                        Join Our Mailing List
+                    </button>
+                </div>
+                <button class="outline rounded-lg h-fit py-3 w-fit px-8 text-l font-medium flex">
+                    Sponsor or Donate
+                </button>
+            </div>
+        </div>
     </div>
-</template>
+
+
+
+</template> 
