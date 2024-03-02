3c33d32f360fcb1ba831aaaae6f9644d1493d664
Luis Diaz <luis.max1001@gmail.com>
2024-03-02 16:42:56 -0600

    Footer work

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,30 @@
 <template>
-    <div class="grid grid-cols-5 gap-1">
-        <img src="@/assets/logo.png" class="p-6">
+    <div class="grid grid-cols-6 gap-1 object-center text-center">
+        <img src="@/assets/logo_black.png" class="w-28">
+        <div class="col-span-2">the blue dog ran really fast and jumped over the fence and became a hero but then he became a villian and was suspected of horrendous crimes.</div>
+        <div class="grid grid-rows-3 gap-3 object-center text-center text-sm">
+            <div class="">Cars</div>
+            <div class="">TAMU SAE</div>
+            <div class="">SAE</div>
+        </div>
+        <div class="grid grid-rows-3 gap-3 object-center text-center text-sm">
+            <div class="">Join the team</div>
+            <div class="">Donate</div>
+            <div class="">Report an issue</div>
+        </div>
+        <div class="grid grid-rows-2 gap-3 object-center text-center text-sm">
+            <div class="">Follow us on social media</div>
+            <div class="grid grid-cols-3 gap-3 object-center text-center text-sm">
+                <div class="">insta</div>
+                <div class="">LinkedIn</div>
+                <div class="">X?</div>
+            </div>
+        </div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+        <div class=""></div>
+
     </div>
 </template>
 
