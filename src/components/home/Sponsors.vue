34e588c1e8c78de66726915f2fe276f4dbeab1bf
Raajheer1 <raaj.patel229@gmail.com>
2024-03-02 15:39:49 -0600

    Add Sponsors

--- src/components/home/Sponsors.vue
+++ src/components/home/Sponsors.vue
@@ -1,32 +1,87 @@
 <template>
   <div class="text-center">
-    <h1 class="font-semibold text-3xl underline-offset-3 underline decoration-aggie-maroon">
-      Proudly sponsored by...
-    </h1>
     <div class="grid grid-cols-8 gap-x-5 my-10">
-      <div class="flex flex-col gap-y-5">
-        <div class="flex rounded-3xl aspect-square" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="https://images.squarespace-cdn.com/content/v1/5ad567678ab72281aed39764/1611538101903-S60WW4HLZGLZ1VYQREV9/GM_Brandmark_2021_Gradient.jpg?format=2500w" class="w-1/2 mx-auto my-auto" />
+      <div class="flex flex-col gap-y-5 mt-56">
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Bendco.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="https://images.squarespace-cdn.com/content/v1/5ad567678ab72281aed39764/1611538101903-S60WW4HLZGLZ1VYQREV9/GM_Brandmark_2021_Gradient.jpg?format=2500w" class="w-1/2 mx-auto my-auto" />
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Dynacraft.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="https://images.squarespace-cdn.com/content/v1/5ad567678ab72281aed39764/1611538101903-S60WW4HLZGLZ1VYQREV9/GM_Brandmark_2021_Gradient.jpg?format=2500w" class="w-1/2 mx-auto my-auto" />
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/GH.png" class="w-1/2 mx-auto my-auto" />
         </div>
-      </div>
-      <div class=""></div>
-      <div class=""></div>
-      <div class="col-span-2 my-auto">
-        <div class="flex rounded-3xl aspect-square" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="https://images.squarespace-cdn.com/content/v1/5ad567678ab72281aed39764/1611538101903-S60WW4HLZGLZ1VYQREV9/GM_Brandmark_2021_Gradient.jpg?format=2500w" class="w-1/2 mx-auto my-auto" />
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Goodwinds.png" class="w-1/2 mx-auto my-auto" />
         </div>
       </div>
-      <div class=""></div>
-      <div class=""></div>
-      <div class=""></div>
+      <div class="flex flex-col gap-y-5 mt-32">
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Tesla.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/BELL.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Perficient.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+      </div>
+      <div class="col-span-4 mt-40">
+        <div class="grid grid-cols-2 gap-x-5">
+          <div class="">
+            <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+              <img src="@/assets/sponsor/Vistra.png" class="w-1/2 mx-auto my-auto" />
+            </div>
+          </div>
+          <div class="my-4">
+            <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+              <img src="@/assets/sponsor/GM.png" class="w-1/2 mx-auto my-auto" />
+            </div>
+          </div>
+        </div>
+        <div class="col-span-4 mt-10">
+          <div class="w-full mx-auto">
+            <h1 class="font-semibold text-6xl">
+              Sponsors
+            </h1>
+            <p class="text-gray-600 my-6 mx-32">
+              We are grateful for the support of our sponsors. Their contributions help us to continue to grow and compete at the highest level. We are always looking for new sponsors to join our team. If you are interested in sponsoring us, please contact us.
+            </p>
+
+            <button class="rounded-xl text-black font-lg py-2 px-6 border hover:bg-black hover:scale-110 hover:text-white transition duration-200 ">
+              Sponsor Us
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-5 mt-32">
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Burns.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Axlebox.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/EControls.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-y-5 mt-56">
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/MEEN.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/Caterpillar.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/GM.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+        <div class="flex rounded-3xl aspect-square bg-off-white" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+          <img src="@/assets/sponsor/GM.png" class="w-1/2 mx-auto my-auto" />
+        </div>
+      </div>
     </div>
   </div>
+  <img src="@/assets/banner.jpg" class="w-full " />
 </template>
 
 <script setup lang="ts">
