bf00173ac52ebd9200bc44b2a5811b871bdba172
Jwu488 <joshuawu158@gmail.com>
2024-03-02 20:42:22 -0600

    removed sponsros

--- src/views/pages/Home.vue
+++ src/views/pages/Home.vue
@@ -24,7 +24,6 @@
 <script setup lang="ts">
   import Landing from '@/components/home/Landing.vue';
   import Stats from "@components/home/Stats.vue";
-  import Sponsors from "@components/home/Sponsors.vue";
 
   import { useMainStore } from "@/store";
   const mainStore = useMainStore();
