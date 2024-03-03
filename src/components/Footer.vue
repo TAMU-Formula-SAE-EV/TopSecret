237fbb9b16198e7421c523770b5b3a1a8f6cf687
Luis Diaz <luis.max1001@gmail.com>
2024-03-02 18:37:46 -0600

    footer stuff

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,12 +8,25 @@
                 :src="currentImage"
                 @mouseover="handleMouseOver"
                 @mouseleave="handleMouseLeave"
-                class="w-40"
+                class="w-20 transition-opacity duration-300"
+                :class="{ 'opacity-50': hovering }"
             >
             <div class="text-2xl text-extrabold">/</div>
-            <img src="@/assets/footer/linkedin.png" class="w-20 mx-auto">
+            <img 
+                :src="currentImage_"
+                @mouseover="handleMouseOver_"
+                @mouseleave="handleMouseLeave_"
+                class="w-20 transition-opacity duration-300"
+                :class="{ 'opacity-50': hovering_ }"
+            >
             <div class="text-2xl text-extrabold">/</div>
-            <img src="@/assets/footer/youtube_bw.png" class="w-20 mx-auto">
+            <img 
+                :src="currentImagez__"
+                @mouseover="handleMouseOver__"
+                @mouseleave="handleMouseLeave__"
+                class="w-20 transition-opacity duration-300"
+                :class="{ 'opacity-50': hovering__ }"
+            >
         </div>
 
         <div class="grid grid-cols-6 gap-1 text-sm text-gray-600">
@@ -29,18 +42,50 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import img1 from '@/assets/footer/insta_bw.png'
+import img2 from '@/assets/footer/insta_color.png'
+import linkedin_c from "@/assets/footer/linkedin_color.png"
+import linkedin_bw from "@/assets/footer/linkedin_bw.png"
+import youtube_color from "@assets/footer/youtube_color.png"
+import youtube_bw from "@/assets/footer/youtube_bw.png"
 
 const hovering = ref(false);
-const currentImage = ref('@/assets/footer/insta.png');
+const currentImage = ref(img1);
 
 const handleMouseOver = () => {
   hovering.value = true;
-  currentImage.value = '@/assets/footer/insta_color.png';
+  currentImage.value = img2;
 };
 
 const handleMouseLeave = () => {
   hovering.value = false;
-  currentImage.value = '@/assets/footer/insta.png';
+  currentImage.value = img1;
+};
+
+const hovering_ = ref(false);
+const currentImage_ = ref(linkedin_bw);
+
+const handleMouseOver_ = () => {
+  hovering_.value = true;
+  currentImage_.value = linkedin_c;
+};
+
+const handleMouseLeave_ = () => {
+  hovering_.value = false;
+  currentImage_.value = linkedin_bw;
+};
+
+const hovering__ = ref(false);
+const currentImage__ = ref(youtube_bw);
+
+const handleMouseOver__ = () => {
+  hovering__.value = true;
+  currentImage__.value = youtube_color;
+};
+
+const handleMouseLeave__ = () => {
+  hovering__.value = false;
+  currentImage__.value = youtube_bw;
 };
 </script>
 
