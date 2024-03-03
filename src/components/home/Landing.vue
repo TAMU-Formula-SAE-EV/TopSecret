c810c410ad4a5e172c01cbf05e1c58ba90d86c98
Jwu488 <joshuawu158@gmail.com>
2024-03-02 20:50:58 -0600

    dark mode edits for mobile firendly

--- src/components/home/Landing.vue
+++ src/components/home/Landing.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <div v-if="mainStore.spooky" class="bg-[url('@/assets/home/crazy-wallpaper.png')] w-full bg-cover pb-16 bg-no-repeat bg-opacity-50">
+    <div v-if="mainStore.spooky" class="bg-[url('@/assets/home/crazy-wallpaper.png')] w-full bg-cover pb-16 bg-no-repeat bg-opacity-50 bg-center">
         <div class="pt-96 pl-12 flex-col flex text-left">
             <p1 class="text-white font-semibold text-4xl pb-2">
                 Texas A&M Formula SAE EV 2024
@@ -8,14 +8,14 @@
             <p1 class="pb-3 text-gray-200">
                 "Design for testing time and quality."
             </p1>
-            <div class="flex-row flex">
-                <div class="pr-4">
-                    <button class="bg-white outline-color-white rounded-2xl h-fit py-3 w-fit px-8 text-l font-medium flex hover:scale-105 transition duration-200"
+            <div class="flex md:flex-row flex-col">
+                <div class="pr-4 mb-4 md:mb-0">
+                    <button class="bg-white outline-color-white rounded-2xl h-fit py-3 w-56 flex justify-center text-m font-medium hover:scale-105 transition duration-200"
                   :class="mainStore.spooky ? 'text-black':''">
                         Sponsor or Donate
                     </button>
                 </div>
-                <button class="outline rounded-2xl h-fit py-3 w-fit px-8 text-l font-medium flex color-white text-white hover:scale-105 transition duration-200">
+                <button class="outline rounded-2xl h-fit py-3 w-56 flex justify-center text-m font-medium color-white text-white hover:scale-105 transition duration-200">
                     Join Our Mailing List
                 </button>
             </div>
