a2c9d5045d3a8ea600329bf6b6da54ba17d0da38
Joshua <joshuawu158@gmail.com>
2024-03-03 14:55:56 -0600

    thicker button

--- src/components/home/Sponsors.vue
+++ src/components/home/Sponsors.vue
@@ -1,89 +1,180 @@
 <template>
   <div class="w-full text-center px-4">
-    <div class="grid grid-cols-2 md:grid-cols-8 space-x-2.5 md:gap-x-5 mt-10 pb-12">
+    <div
+      class="grid grid-cols-2 md:grid-cols-8 space-x-2.5 md:gap-x-5 mt-10 pb-12"
+    >
       <div class="flex flex-col gap-y-5 mt-12 md:mt-56">
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Bendco.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Bendco.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Dynacraft.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Dynacraft.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
           <img src="@/assets/sponsor/GH.png" class="w-1/2 mx-auto my-auto" />
         </div>
       </div>
       <div class="flex flex-col gap-y-5 mt-8 md:mt-32">
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
           <img src="@/assets/sponsor/Tesla.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
           <img src="@/assets/sponsor/BELL.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Perficient.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Perficient.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
       </div>
       <div class="col-span-4 mt-12 md:mt-40">
         <div class="grid grid-cols-2 gap-x-5">
           <div class="">
-            <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-              <img src="@/assets/sponsor/Vistra.png" class="w-1/2 mx-auto my-auto" />
+            <div
+              class="flex rounded-3xl aspect-square"
+              :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+              style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+            >
+              <img
+                src="@/assets/sponsor/Vistra.png"
+                class="w-1/2 mx-auto my-auto"
+              />
             </div>
           </div>
           <div class="my-4">
-            <div class="flex rounded-3xl aspect-square " :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-              <img src="@/assets/sponsor/GM.png" class="w-1/2 mx-auto my-auto" />
+            <div
+              class="flex rounded-3xl aspect-square"
+              :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+              style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+            >
+              <img
+                src="@/assets/sponsor/GM.png"
+                class="w-1/2 mx-auto my-auto"
+              />
             </div>
           </div>
         </div>
         <div class="hidden md:block col-span-4 mt-10">
           <div class="w-full mx-auto">
-            <h1 class="font-semibold text-6xl">
-              Sponsors
-            </h1>
+            <h1 class="font-semibold text-6xl">Sponsors</h1>
             <p class="text-gray-600 my-6 mx-32">
-              We are grateful for the support of our sponsors. Their contributions help us to continue to grow and compete at the highest level. We are always looking for new sponsors to join our team. If you are interested in sponsoring us, please contact us.
+              We are grateful for the support of our sponsors. Their
+              contributions help us to continue to grow and compete at the
+              highest level. We are always looking for new sponsors to join our
+              team. If you are interested in sponsoring us, please contact us.
             </p>
 
-            <button class="rounded-xl font-lg py-2 px-6 border hover:bg-black hover:scale-110 hover:text-white transition duration-200">
+            <button
+              class="rounded-xl font-lg py-2 px-6 border border-gray-300 border-2 hover:border-black hover:bg-black hover:scale-110 hover:text-white transition duration-200"
+            >
               Sponsor Us
             </button>
           </div>
         </div>
       </div>
       <div class="flex flex-col gap-y-5 mt-8 md:mt-32">
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
           <img src="@/assets/sponsor/Burns.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Axlebox.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Axlebox.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/EControls.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/EControls.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
       </div>
       <div class="flex flex-col gap-y-5 mt-12 md:mt-56">
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
           <img src="@/assets/sponsor/MEEN.png" class="w-1/2 mx-auto my-auto" />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Caterpillar.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Caterpillar.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
-        <div class="flex rounded-3xl aspect-square" :class="mainStore.spooky ? 'bg-zinc-900':'bg-off-white'" style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1);">
-          <img src="@/assets/sponsor/Goodwinds.png" class="w-1/2 mx-auto my-auto" />
+        <div
+          class="flex rounded-3xl aspect-square"
+          :class="mainStore.spooky ? 'bg-zinc-900' : 'bg-off-white'"
+          style="box-shadow: 0 0 5px 2px rgba(0, 0, 0, 0.1)"
+        >
+          <img
+            src="@/assets/sponsor/Goodwinds.png"
+            class="w-1/2 mx-auto my-auto"
+          />
         </div>
       </div>
     </div>
     <div class="md:hidden mt-10">
       <div class="w-full mx-auto">
-        <h1 class="font-semibold text-6xl">
-          Sponsors
-        </h1>
-        <p class="text-gray-600 my-6 ">
-          We are grateful for the support of our sponsors. Their contributions help us to continue to grow and compete at the highest level. We are always looking for new sponsors to join our team. If you are interested in sponsoring us, please contact us.
+        <h1 class="font-semibold text-6xl">Sponsors</h1>
+        <p class="text-gray-600 my-6">
+          We are grateful for the support of our sponsors. Their contributions
+          help us to continue to grow and compete at the highest level. We are
+          always looking for new sponsors to join our team. If you are
+          interested in sponsoring us, please contact us.
         </p>
 
-        <button class="rounded-xl font-lg py-2 px-6 border hover:bg-black hover:scale-110 hover:text-white transition duration-200">
+        <button
+          class="rounded-xl font-lg py-2 px-6 border hover:bg-black hover:scale-110 hover:text-white transition duration-200"
+        >
           Sponsor Us
         </button>
       </div>
