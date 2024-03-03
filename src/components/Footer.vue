5a4826ef22982cd288343a8c58e07640eabe35ed
Luis Diaz <luis.max1001@gmail.com>
2024-03-02 18:03:35 -0600

    footer changes

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,47 @@
 <template>
-    <div class="grid grid-cols-6 gap-1 object-center text-center">
-        <img src="@/assets/logo_black.png" class="w-28">
-        <div class="col-span-2">the blue dog ran really fast and jumped over the fence and became a hero but then he became a villian and was suspected of horrendous crimes.</div>
-        <div class="grid grid-rows-3 gap-3 object-center text-center text-sm">
+    <div class="grid grid-row-3 mt-7 mb-7 gap-3">
+
+        <img src="@/assets/footer/logo_black.png" class="mx-auto w-40">
+
+        <div class="flex items-center justify-center space-x-12">
+            <img 
+                :src="currentImage"
+                @mouseover="handleMouseOver"
+                @mouseleave="handleMouseLeave"
+                class="w-40"
+            >
+            <div class="text-2xl text-extrabold">/</div>
+            <img src="@/assets/linkedin.png" class="w-20 mx-auto">
+            <div class="text-2xl text-extrabold">/</div>
+            <img src="@/assets/youtube_bw.png" class="w-20 mx-auto">
+        </div>
+
+        <div class="grid grid-cols-6 gap-1 text-sm text-gray-600">
             <div class="">Cars</div>
             <div class="">TAMU SAE</div>
             <div class="">SAE</div>
-        </div>
-        <div class="grid grid-rows-3 gap-3 object-center text-center text-sm">
             <div class="">Join the team</div>
             <div class="">Donate</div>
             <div class="">Report an issue</div>
         </div>
-        <div class="grid grid-rows-2 gap-3 object-center text-center text-sm">
-            <div class="">Follow us on social media</div>
-            <div class="grid grid-cols-3 gap-3 object-center text-center text-sm">
-                <div class="">insta</div>
-                <div class="">LinkedIn</div>
-                <div class="">X?</div>
-            </div>
-        </div>
-        <div class=""></div>
-        <div class=""></div>
-        <div class=""></div>
-        <div class=""></div>
-
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
+const hovering = ref(false);
+const currentImage = ref('@/assets/footer/insta.png');
+
+const handleMouseOver = () => {
+  hovering.value = true;
+  currentImage.value = '@/assets/footer/insta_color.png';
+};
+
+const handleMouseLeave = () => {
+  hovering.value = false;
+  currentImage.value = '@/assets/footer/insta.png';
+};
 </script>
 
 <style scoped>
