656699b5e43f71e6f9edf26ab9f71e4eef3a4151
Jwu488 <joshuawu158@gmail.com>
2024-03-02 21:13:23 -0600

    footer darkmode

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="grid grid-row-3 mt-7 mb-7 gap-3">
+    <div v-if="mainStore.spooky" class="grid grid-row-3 pt-7 pb-7 gap-3 bg-black">
 
-        <img src="@/assets/footer/logo_black.png" class="mx-auto w-40">
+        <img src="@/assets/footer/logo_black.png" class="mx-auto  w-40">
 
         <div class="flex items-center justify-center space-x-8">
             <img 
@@ -29,7 +29,7 @@
             >
         </div>
 
-        <div class="grid grid-cols-6 gap-1 text-xs md:text-sm text-gray-600 text-center pr-4">
+        <div class="grid grid-cols-6 gap-1 text-xs md:text-sm text-white text-gray-600 text-center pr-4">
             <div class="">Cars</div>
             <div class="">TAMU SAE</div>
             <div class="">SAE</div>
@@ -38,9 +38,54 @@
             <div class="">Report an issue</div>
         </div>
     </div>
+
+<div v-else class="grid grid-row-3 mt-7 mb-7 gap-3">
+
+  <img src="@/assets/footer/logo_black.png" class="mx-auto w-40">
+
+  <div class="flex items-center justify-center space-x-8">
+      <img 
+          :src="currentImage"
+          @mouseover="handleMouseOver"
+          @mouseleave="handleMouseLeave"
+          class="w-20 transition-opacity duration-300"
+          :class="{ 'opacity-50': hovering }"
+      >
+      <div class="text-2xl text-extrabold">/</div>
+      <img 
+          :src="currentImage_"
+          @mouseover="handleMouseOver_"
+          @mouseleave="handleMouseLeave_"
+          class="w-20 transition-opacity duration-300"
+          :class="{ 'opacity-50': hovering_ }"
+      >
+      <div class="text-2xl text-extrabold">/</div>
+      <img 
+          :src="currentImage__"
+          @mouseover="handleMouseOver__"
+          @mouseleave="handleMouseLeave__"
+          class="w-20 transition-opacity duration-300"
+          :class="{ 'opacity-50': hovering__ }"
+      >
+  </div>
+
+  <div class="grid grid-cols-6 gap-1 text-xs md:text-sm text-gray-600 text-center pr-4">
+      <div class="">Cars</div>
+      <div class="">TAMU SAE</div>
+      <div class="">SAE</div>
+      <div class="">Join the team</div>
+      <div class="">Donate</div>
+      <div class="">Report an issue</div>
+  </div>
+</div>
 </template>
 
 <script setup lang="ts">
+
+import { useMainStore } from "@/store";
+
+const mainStore = useMainStore();
+
 import { ref } from 'vue';
 import img1 from '@/assets/footer/insta_bw.png'
 import img2 from '@/assets/footer/insta_color.png'
