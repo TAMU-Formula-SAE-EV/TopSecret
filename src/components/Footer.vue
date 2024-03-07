5f9b1bb0493a95308abe8958bbf66670f1e17bb7
Luis Diaz <luis.max1001@gmail.com>
2024-03-06 18:23:48 -0600

    Footer spooky enabled

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,36 @@
 <template>
   <div v-if="mainStore.spooky" class="grid grid-row-3 pt-7 pb-7 gap-3 bg-black">
 
-    <img src="@/assets/footer/logo_black.png" class="mx-auto  w-40">
+    <img src="@/assets/logo.png" class="mx-auto  w-40">
 
     <div class="flex items-center justify-center space-x-8">
       <a href="https://www.instagram.com/tamuformulaelectric/">
       <img
-          :src="currentImage"
-          @mouseover="handleMouseOver"
-          @mouseleave="handleMouseLeave"
+          :src="currentImageSpook"
+          @mouseover="handleMouseOverSpook"
+          @mouseleave="handleMouseLeaveSpook"
           class="w-20 transition-opacity duration-300"
-          :class="{ 'opacity-50': hovering }"
+          :class="{ 'opacity-50': hoveringSpook }"
       >
       </a>
-      <div class="text-2xl text-extrabold">/</div>
+      <div class="text-2xl text-extrabold text-white">/</div>
       <a href="https://www.linkedin.com/company/texas-a-m-fsae-electric-racing/mycompany/">
       <img
-          :src="currentImage_"
-          @mouseover="handleMouseOver_"
-          @mouseleave="handleMouseLeave_"
+          :src="currentImageSpook_"
+          @mouseover="handleMouseOverSpook_"
+          @mouseleave="handleMouseLeaveSpook_"
           class="w-20 transition-opacity duration-300"
-          :class="{ 'opacity-50': hovering_ }"
+          :class="{ 'opacity-50': hoveringSpook_ }"
       >
       </a>
-      <div class="text-2xl text-extrabold">/</div>
+      <div class="text-2xl text-extrabold text-white">/</div>
       <a href="https://www.youtube.com/@tamuformulaelectric/featured">
       <img
-          :src="currentImage__"
-          @mouseover="handleMouseOver__"
-          @mouseleave="handleMouseLeave__"
+          :src="currentImageSpook__"
+          @mouseover="handleMouseOverSpook__"
+          @mouseleave="handleMouseLeaveSpook__"
           class="w-20 transition-opacity duration-300"
-          :class="{ 'opacity-50': hovering__ }"
+          :class="{ 'opacity-50': hoveringSpook__ }"
       >
       </a>
     </div>
@@ -105,6 +105,9 @@ import linkedin_c from "@/assets/footer/linkedin_color.png"
 import linkedin_bw from "@/assets/footer/linkedin_bw.png"
 import youtube_color from "@assets/footer/youtube_color.png"
 import youtube_bw from "@/assets/footer/youtube_bw.png"
+import insta_spooky from "@assets/footer/insta_spooky.png"
+import youtube_spooky from "@assets/footer/youtube_spooky.png"
+import linkedin_spooky from "@assets/footer/linkedin_spooky.png"
 
 const hovering = ref(false);
 const currentImage = ref(img1);
@@ -144,6 +147,46 @@ const handleMouseLeave__ = () => {
   hovering__.value = false;
   currentImage__.value = youtube_bw;
 };
+
+const hoveringSpook = ref(false);
+const currentImageSpook = ref(insta_spooky);
+
+const handleMouseOverSpook = () => {
+  hoveringSpook.value = true;
+  currentImageSpook.value = img2;
+};
+
+const handleMouseLeaveSpook = () => {
+  hoveringSpook.value = false;
+  currentImageSpook.value = insta_spooky;
+};
+
+const hoveringSpook_ = ref(false);
+const currentImageSpook_ = ref(linkedin_spooky);
+
+const handleMouseOverSpook_ = () => {
+  hoveringSpook_.value = true;
+  currentImageSpook_.value = linkedin_c;
+};
+
+const handleMouseLeaveSpook_ = () => {
+  hoveringSpook_.value = false;
+  currentImageSpook_.value = linkedin_spooky;
+};
+
+const hoveringSpook__ = ref(false);
+const currentImageSpook__ = ref(youtube_spooky);
+
+const handleMouseOverSpook__ = () => {
+  hoveringSpook__.value = true;
+  currentImageSpook__.value = youtube_color;
+};
+
+const handleMouseLeaveSpook__ = () => {
+  hoveringSpook__.value = false;
+  currentImageSpook__.value = youtube_spooky;
+};
+
 </script>
 
 <style scoped>
